4f541b1d4b32748e6e25c281b0f4e37618363114
Neha-Jacob <60283339+Neha-Jacob@users.noreply.github.com>
2026-08-27 17:22:39 +0200

    solution v1
    Runtime
    - 241ms
    - Beats 11.59%
    
    Memory
    - 14.4 MB
    - Beats 6.53%
    
    Time complexity: O(n * k log k)
    
    Space complexity: O(n * k)
    
    n is the number of strings in the input list, and k is the average length of each string.

---
+++ group_anagrams.py
@@ -0,0 +1,62 @@
+"""
+# Group Anagrams
+
+Given an array of strings strs, group all anagrams together into sublists.
+You may return the output in any order.
+
+An anagram is a string that contains the exact same characters as another
+string, but the order of the characters can be different.
+
+## Example 1
+
+```text
+Input: strs = ["act","pots","tops","cat","stop","hat"]
+
+Output: [["hat"],["act", "cat"],["stop", "pots", "tops"]]
+```
+
+## Example 2
+
+```text
+Input: strs = ["x"]
+
+Output: [["x"]]
+```
+
+## Example 3
+
+```text
+Input: strs = [""]
+
+Output: [[""]]
+```
+
+## Constraints
+
+- 1 <= strs.length <= 10000.
+- 0 <= strs[i].length <= 100
+- strs[i] is made up of lowercase English letters.
+
+## Topics
+
+- Array
+- Hash Table
+- String
+- Sorting
+"""
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        sorts = {}
+        for i in range(len(strs)):
+            key = "".join(sorted(strs[i]))
+            if key not in sorts:
+                sorts[key] = []
+            sorts[key].append(i)
+        new_list = []
+        for value in sorts.values():
+            sub_list = []
+            for i in value:
+                sub_list.append(strs[i])
+            new_list.append(sub_list)
+        return new_list
